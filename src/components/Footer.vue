0ed2bd4c8f8c3cba993911e30bf7dec0eed10afe
Dima Polskoy <polskoydm@gmail.com>
2023-05-17 18:18:19 -0600

    Cart

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,10 +11,12 @@
           <i class="fa-solid fa-calendar fa-lg"></i>
           <span class="footer-button-label">Saved</span>
         </a>
-        <a href="/shopping_cart" class="footer-button">
-          <i class="fa-solid fa-cart-shopping fa-lg"></i>
-          <span class="footer-button-label">Cart ({{ cartItems.length }})</span>
-        </a>
+   
+          <a href="/shopping_cart" class="footer-button">
+  <i class="fa-solid fa-cart-shopping fa-lg"></i>
+  <span class="footer-button-label">Cart</span>
+  <span class="cart-counter">{{ cartItems.length }}</span>
+</a>
         <a href="/user_login" class="footer-button">
           <i class="fa-solid fa-user fa-lg"></i>
           <span class="footer-button-label">Profile</span>
