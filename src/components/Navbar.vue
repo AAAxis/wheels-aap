a8f9a220a9d6027e98f850ca849593c52c18cb5e
Dima Polskoy <polskoydm@gmail.com>
2023-05-19 14:27:45 -0600

    new

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,24 +6,16 @@
         <span class="footer-button-label">Home</span>
       </a>
    
-      <a href="/saved" v-if="!getCookie('email')" class="footer-button">
+      <a href="/saved" class="footer-button">
         <i class="fa-solid fa-calendar fa-lg"></i>
         <span class="footer-button-label">Saved</span>
       </a>
-      <a href="/shoping_cart" class="footer-button" v-if="!getCookie('email')">
+      <a href="/shoping_cart" class="footer-button">
         <i class="fa-solid fa-cart-shopping fa-lg"></i>
         <span class="footer-button-label">Cart</span>
         <span class="cart-counter">{{ cartItems.length }}</span>
       </a>
-      <a href="https://polskoydm.pythonanywhere.com/user_login" v-if="getCookie('email')" class="footer-button">
-        <i class="fa-solid fa-calendar fa-lg"></i>
-        <span class="footer-button-label">Saved</span>
-      </a>
-      <a href="https://polskoydm.pythonanywhere.com/user_login" class="footer-button" v-if="getCookie('email')">
-        <i class="fa-solid fa-cart-shopping fa-lg"></i>
-        <span class="footer-button-label">Cart</span>
-        <span class="cart-counter">{{ cartItems.length }}</span>
-      </a>
+      
       <a @click="openNav" class="footer-button">
         <i class="fa-solid fa-user fa-lg"></i>
         <span class="footer-button-label">Profile</span>
@@ -48,14 +40,12 @@
 </template>
 
 <script>
-
 export default {
-
   props: {
     cartItems: {
       type: Array,
       required: true
-    },
+    }
   },
   data() {
     return {
@@ -65,41 +55,36 @@ export default {
     };
   },
   methods: {
-  // Other methods
-  
-  openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-  },
-  closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  },
-  logout() {
-    // Implement logout functionality
-  },
-  getCookie(name) {
-    const cookieString = document.cookie;
-    const cookies = cookieString.split(';');
+    openNav() {
+      document.getElementById("mySidenav").style.width = "100%";
+    },
+    closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    },
+    logout() {
+      // Implement logout functionality
+    },
+    getCookie(name) {
+      const cookieString = document.cookie;
+      const cookies = cookieString.split(';');
 
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.startsWith(name + '=')) {
-        return decodeURIComponent(cookie.substring(name.length + 1));
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.startsWith(name + '=')) {
+          return decodeURIComponent(cookie.substring(name.length + 1));
+        }
       }
-    }
 
-    return null;
+      return null;
+    },
+    setUserData() {
+      this.userEmail = this.getCookie('email');
+      this.userImage = this.getCookie('image');
+      this.userName = this.getCookie('username');
+    }
   },
-  setUserData() {
-    this.userEmail = this.getCookie('email');
-    this.userImage = this.getCookie('image');
-    this.userName = this.getCookie('username');
-  }
-},
   created() {
     this.setUserData();
   }
-
-   
-
 };
 </script>
