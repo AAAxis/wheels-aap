ba53641c06d2b854819ef47ac9a9419addd97674
Dima Polskoy <polskoydm@gmail.com>
2023-05-20 22:15:40 -0600

    response

--- src/components/History.vue
+++ src/components/History.vue
@@ -28,18 +28,25 @@
     methods: {
       fetchOrders(email) {
         // Send a GET request with the email as a query parameter
-        axios.get('https://polskoydm.pythonanywhere.com/history', { params: { email } })
+        axios
+          .get('https://polskoydm.pythonanywhere.com/history', { params: { email } })
           .then(response => {
             // Assign the received data to the component's data property
             this.orders = response.data.orders;
           })
           .catch(error => {
-            console.error('Error:', error);
+            if (error.response) {
+              // Handle the error when a response with a non-2xx status code is received
+              console.error('Error:', error.response.data.message);
+            } else {
+              // Handle other types of errors
+              console.error('Error:', error.message);
+            }
           });
       },
       getCartItems(order) {
         // Extract the cart items for the given order
-        const cartItems = data.cart_items.find(cart => cart.order_id === order.id);
+        const cartItems = order.cart_items;
         return cartItems || [];
       },
     },
