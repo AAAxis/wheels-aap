e532d5a44dd7dec566097a09c057bca54a10ba18
Dima Polskoy <polskoydm@gmail.com>
2023-05-20 22:25:21 -0600

    histort

--- src/components/History.vue
+++ src/components/History.vue
@@ -20,28 +20,17 @@
       };
     },
     mounted() {
-      const email = localStorage.getItem('email');
-      if (email) {
-        this.fetchOrders(email);
-      }
+      this.fetchOrders();
     },
     methods: {
-      fetchOrders(email) {
-        // Send a GET request with the email as a query parameter
+      fetchOrders() {
         axios
-          .get('https://polskoydm.pythonanywhere.com/history', { params: { email } })
+          .get('https://polskoydm.pythonanywhere.com/history')
           .then(response => {
-            // Assign the received data to the component's data property
             this.orders = response.data.orders;
           })
           .catch(error => {
-            if (error.response) {
-              // Handle the error when a response with a non-2xx status code is received
-              console.error('Error:', error.response.data.message);
-            } else {
-              // Handle other types of errors
-              console.error('Error:', error.message);
-            }
+            console.error('Error:', error);
           });
       },
     },
