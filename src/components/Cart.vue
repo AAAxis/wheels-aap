8677d92fb8191bbb629354b80237435a427909d1
Dima Polskoy <polskoydm@gmail.com>
2023-05-20 21:01:38 -0600

    yui

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -4,7 +4,7 @@
     <ul>
       <li v-for="item in cartItems" :key="item.id">
         <div class="cart-item">
-          <img :src="getImageUrl(item.image)" :alt="item.name" class="cart-item-image">
+          <img :src="item.image" :alt="item.name" class="cart-item-image">
           <div class="cart-item-details">
             <span class="cart-item-name">{{ item.name }}</span>
             <div class="cart-item-quantity">
@@ -24,10 +24,6 @@
 
 <script>
 export default {
-
-  getImageUrl(image) {
-      return `https://polskoydm.pythonanywhere.com/static/uploads/${image}`;
-    },
 
   props: {
     cartItems: {
